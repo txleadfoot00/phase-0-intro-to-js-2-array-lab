845345f5e7347ce17ad63d9172aefa74cd06c1b6
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-16 21:34:55 -0700

    commit tree 490e1c8b9e5c01eca194ee1c86270bef9b6a4333

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name = "Ralph") {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name = "Bob") {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+    
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let appendedCat = [...cats, name];
+    return (appendedCat);
+}
+
+function prependCat(name) {
+    let prependedCat = [name, ...cats];
+    return (prependedCat);
+}
+
+function removeLastCat() {
+    let removedLastCat = cats.slice(0,-1) 
+    return(removedLastCat);
+    
+}
+function removeFirstCat() {
+    let removedFirstCat = cats.slice(1) 
+    return (removedFirstCat);
+}
